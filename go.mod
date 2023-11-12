57d9d6f67e699f353e0373df37fe58ba63a49acf
mancuoj <1337381768@qq.com>
2023-11-13 00:08:45 +0800

    update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mancuoj/snippetbox
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("Hello from Snippetbox..."))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
+
+	w.Write([]byte("Create a snippet..."))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Print("Starting server on http://localhost:4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
