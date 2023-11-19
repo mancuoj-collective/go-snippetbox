3a337ed6259710f6b160459410cc609053b48747
mancuoj <1337381768@qq.com>
2023-11-19 22:26:51 +0800

    form decoder

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/go-playground/form"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/mancuoj/snippetbox/internal/models"
 )
@@ -17,6 +18,7 @@ type application struct {
 	errorLog      *log.Logger
 	snippets      *models.SnippetModel
 	templateCache map[string]*template.Template
+	formDecoder   *form.Decoder
 }
 
 func main() {
@@ -38,11 +40,14 @@ func main() {
 		errorLog.Fatal(err)
 	}
 
+	formDecoder := form.NewDecoder()
+
 	app := &application{
 		infoLog:       infoLog,
 		errorLog:      errorLog,
 		snippets:      &models.SnippetModel{DB: db},
 		templateCache: templateCache,
+		formDecoder:   formDecoder,
 	}
 
 	srv := &http.Server{
--- go.mod
+++ go.mod
@@ -5,3 +5,5 @@ go 1.21.4
 require github.com/go-sql-driver/mysql v1.7.1
 
 require github.com/julienschmidt/httprouter v1.3.0
+
+require github.com/go-playground/form v3.1.4+incompatible // indirect
