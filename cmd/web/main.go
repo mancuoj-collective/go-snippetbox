afaf1beda40f0369e822da40735f9a17587822b1
mancuoj <mancuoj@gmail.com>
2025-01-01 01:47:10 +0800

    feat: parse tmpl, static file server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,11 +7,15 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("Starting server on http://localhost:4000")
+	log.Print("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
